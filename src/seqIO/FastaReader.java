79db4751cd30a6a0c681de1b198c707724267a50
Jonathan Schmitz <jonathanschmitz@posteo.net>
2018-03-04 17:01:58 +0100

    Use sequence IDs

--- src/seqIO/FastaReader.java
+++ src/seqIO/FastaReader.java
@@ -12,45 +12,48 @@ import seq.Sequence;
 import seq.Utils;
 
 public class FastaReader {
-	private static Map<String, Sequence> read(String fastaPath) {
-		Map<String, Sequence> seqMap = new HashMap<String, Sequence>();
-		List<String> keys = new ArrayList<String>();
+	private static ArrayList<Sequence> read(String fastaPath) {
+		ArrayList<Sequence> seqs = new ArrayList<Sequence>();
 		String key = "";
+		ArrayList<Sequence> tmpList = new ArrayList<Sequence>();
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(fastaPath));
 			String line;
 			while ((line = reader.readLine()) != null) {
 				if (line.charAt(0) == '>') {
 					if (!key.isEmpty()) {
-						// save the previous entry!
-
+						saveSeq(key, tmpList);
+						tmpList = new ArrayList<Sequence>();
 					}
 					key = line.split(" ")[0].substring(1);
+				} else {
+					tmpList.add(new Sequence(line.trim()));
 				}
 			}
+			saveSeq(key, tmpList);
 			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return seqMap;
+		return seqs;
 	}
 	
-	void saveSeq(String key, ArrayList<Sequence> seqList, Map<String, Sequence> map) {
+	private static void saveSeq(String key, ArrayList<Sequence> seqList) {
+		System.out.println(key);
 		Sequence seq = Utils.join(seqList);
-		map.put(key, seq);
+		seq.setId(key);
+		seqList.add(seq);
 	}
 
 	public static void main(String[] args) {
-
-		String[] args2 = { "/Users/jonathanschmitz/stuff/java-test/test.fasta" };
 		if (args.length != 1) {
-			// System.out.println("USAGE: FastaReader <fasta_path>");
+			System.out.println("USAGE: FastaReader <fasta_path>");
 		}
-		String fastaPath = args2[0];
+		String fastaPath = args[0];
 		System.out.println(fastaPath);
-		List<String> ids = read(fastaPath);
-		for (String id : ids) {
-			System.out.println(id);
+		ArrayList<Sequence> seqs = read(fastaPath);
+		for (Sequence seq: seqs) {
+			System.out.println(seq.getId() + ": " + seq.getSeq());
 		}
 	}
 }
