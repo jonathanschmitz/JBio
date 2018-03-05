9fc28fb6b0ec288fe5abcbe9df24838b6a7bb6cb
Jonathan Schmitz <jonathanschmitz@posteo.net>
2018-03-05 09:34:53 +0100

    Add hydrophobicity table

---
+++ src/seq/hydrophobicityTable.java
@@ -0,0 +1,33 @@
+package seq;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class hydrophobicityTable {
+	private static String tablePath = TranslateTable.class.getResource("../StaticFiles/hydrophobicity.csv").getFile();
+	public static final Map<Character, Float> hydroTable;
+	static {
+		hydroTable = new HashMap<Character, Float>();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(tablePath));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] lineSplit = line.split(",");
+				Character aa = lineSplit[0].charAt(0);
+				Float hydro = Float.parseFloat(lineSplit[1]);
+				hydroTable.put(aa, hydro);
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public Float get(Character aa) {
+		return hydroTable.get(aa);
+	}
+}
